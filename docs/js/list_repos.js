56d5e29e349a53ff7e8ab3bcb9afebea8cc4cd7a
kai5041 <kai5041contact@gmail.com>
2025-11-02 23:27:16 +0100

    Tweaks

---
+++ docs/js/list_repos.js
@@ -0,0 +1,24 @@
+async function list_repos(org_slug, org_display_name) {
+  const res = await fetch(`https://api.github.com/orgs/${org_slug}/repos?per_page=100`);
+  const data = await res.json();
+  const container = document.getElementById("projects");
+
+  const header = document.createElement("h1");
+  header.textContent = `${org_display_name} Projects`;
+
+  container.appendChild(header);
+
+  data.forEach(repo => {
+    const div = document.createElement("div");
+    const link = document.createElement("a");
+    link.href = repo.html_url;
+    link.textContent = repo.name;
+    link.style.fontWeight = "bold";
+    const text = document.createTextNode(`: ${repo.description || ""}`);
+    div.appendChild(link);
+    div.appendChild(text);
+    container.appendChild(div);
+  });
+
+  container.appendChild(document.createElement("hr"));
+}
